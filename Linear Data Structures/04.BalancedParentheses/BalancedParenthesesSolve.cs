177d61a9d32981c95405473020b6ac615d93618c
Yordan Ivanov <yordangoshovivanov@gmail.com>
2022-02-26 14:51:00 +0200

    Add files via upload

---
+++ Linear Data Structures/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,80 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            var input = parentheses;
+            var queue = new Queue<char>(input);
+
+            int counter = 0;
+            
+            if (queue.Count % 2 != 0)
+            {
+                return false;
+            }
+
+            while (queue.Count != 0)
+            {
+                var first = queue.Dequeue();
+                var next = queue.Peek();
+
+                if (first == '{')
+                {
+                    if (next == '}')
+                    {
+                        queue.Dequeue();
+                        counter = 0;
+                        continue;
+                    }
+                    else
+                    {
+                        queue.Enqueue(first);
+                    }
+                }
+                else if (first == '(')
+                {
+                    if (next == ')')
+                    {
+                        queue.Dequeue();
+                        counter = 0;
+                        continue;
+                    }
+                    else
+                    {
+                        queue.Enqueue(first);
+                    }
+                }
+                else if (first == '[')
+                {
+                    if (next == ']')
+                    {
+                        queue.Dequeue();
+                        counter = 0;
+                        continue;
+                    }
+                    else
+                    {
+                        queue.Enqueue(first);
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(first);
+                }
+
+                counter++;
+
+                if (counter == queue.Count)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
