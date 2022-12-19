248794a5d778d2f38b33180b427af36e40ec1b55
Yordan Ivanov <yordangoshovivanov@gmail.com>
2022-12-19 20:58:08 +0200

    Add files via upload

---
+++ 02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/01.Hierarchy/Hierarchy.cs
@@ -0,0 +1,126 @@
+﻿namespace _01.Hierarchy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.Linq;
+
+    public class Hierarchy<T> : IHierarchy<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+                Children = new List<Node>();
+            }
+            public T Value { get; set; }
+            public List<Node> Children { get; set; }
+        }
+
+        private Node root;
+        private Dictionary<T, Node> nodesByValue;
+        private Dictionary<T, Node> parentsByChild;
+
+        public Hierarchy(T item)
+        {
+            root = new Node(item);
+            nodesByValue = new Dictionary<T, Node>();
+            parentsByChild = new Dictionary<T, Node>();
+            nodesByValue.Add(item, root);
+            parentsByChild.Add(item, null);
+        }
+
+        public int Count => nodesByValue.Count;
+
+        public void Add(T element, T child)
+        {
+            if (!nodesByValue.ContainsKey(element) || !nodesByValue.ContainsKey(child))
+            {
+                throw new ArgumentException();
+            }
+ 
+            var node = new Node(child);
+            nodesByValue.Add(child, node);
+            parentsByChild.Add(child, nodesByValue[element]);
+            nodesByValue[element].Children.Add(node);
+        }
+        public void Remove(T element)
+        {
+            if (root.Value.Equals(element))
+            {
+                throw new InvalidOperationException();
+            }
+            else if (!nodesByValue.ContainsKey(element))
+            {
+                throw new ArgumentException();
+            }
+
+            var parent = parentsByChild[element];
+            var node = nodesByValue[element];
+            parent.Children.Remove(node);
+            parent.Children.AddRange(node.Children);
+
+            foreach (var child in node.Children)
+            {
+                parentsByChild[child.Value] = parent;
+            }
+
+            nodesByValue.Remove(element);
+            parentsByChild.Remove(element);
+        }
+
+        public IEnumerable<T> GetChildren(T element)
+        {
+            if (!nodesByValue.ContainsKey(element))
+            {
+                throw new ArgumentException();
+            }
+
+            return nodesByValue[element].Children.Select(node => node.Value);
+        }
+
+        public T GetParent(T element)
+        {
+            if (!nodesByValue.ContainsKey(element))
+            {
+                throw new ArgumentException();
+            }
+
+            var parent = parentsByChild[element].Value != null ? parentsByChild[element].Value : default;
+            return parent;
+        }
+
+        public bool Contains(T element)
+        {
+            return nodesByValue.ContainsKey(element);
+        }
+
+        public IEnumerable<T> GetCommonElements(Hierarchy<T> other)
+        {
+            return nodesByValue.Keys.Intersect(other.nodesByValue.Keys);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+            
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                yield return node.Value;
+
+                foreach (var child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/02. B-Trees-2-3-Trees-and-AVL-Trees-Exercise-Skeleton/02.Two-Three/TwoThreeTree.cs
@@ -0,0 +1,173 @@
+﻿namespace _02.Two_Three
+{
+    using System;
+    using System.Text;
+
+    public class TwoThreeTree<T> where T : IComparable<T>
+    {
+        private TreeNode<T> root;
+
+        public void Insert(T key)
+        {
+            root = Insert(root, key);
+        }
+
+        private TreeNode<T> Insert(TreeNode<T> node, T element)
+        {
+            if (node == null)
+            {
+                return new TreeNode<T>(element);
+            }
+
+            if (node.IsLeaf())
+            {
+                return MergeElement(node, new TreeNode<T>(element));
+            }
+
+            TreeNode<T> returnNode;
+            if (element.CompareTo(node.LeftKey) < 0)
+            {
+                returnNode = Insert(node.LeftChild, element);
+                if (returnNode == node.LeftChild)
+                {
+                    return node;
+                }
+                else
+                {
+                    return MergeElement(node, returnNode);
+                }
+            }
+            else if (node.IsTwoNode() || element.CompareTo(node.RightKey) < 0)
+            {
+                returnNode = Insert(node.MiddleChild, element);
+                if (returnNode == node.MiddleChild)
+                {
+                    return node;
+                }
+                else
+                {
+                    return MergeElement(node, returnNode);
+                }
+            }
+            else
+            {
+                returnNode = Insert(node.RightChild, element);
+                if (returnNode == node.RightChild)
+                {
+                    return node;
+                }
+                else
+                {
+                    return MergeElement(node, returnNode);
+                }
+            }
+        }
+
+        private TreeNode<T> MergeElement(TreeNode<T> currNode, TreeNode<T> node)
+        {
+            if (currNode.IsTwoNode())
+            {
+                if (currNode.LeftKey.CompareTo(node.LeftKey) < 0)
+                {
+                    currNode.RightKey = node.LeftKey;
+                    currNode.MiddleChild = node.LeftChild;
+                    currNode.RightChild = node.MiddleChild;
+                }
+                else
+                {
+                    currNode.RightKey = currNode.LeftKey;
+                    currNode.RightChild = currNode.MiddleChild;
+                    currNode.LeftKey = node.LeftKey;
+                    currNode.MiddleChild = node.MiddleChild;
+                }
+
+                return currNode;
+            }
+            else if (currNode.LeftKey.CompareTo(node.LeftKey) >= 0)
+            {
+                var newNode = new TreeNode<T>(currNode.LeftKey)
+                {
+                    LeftChild = node,
+                    MiddleChild = currNode
+                };
+
+                node.LeftChild = currNode.LeftChild;
+                currNode.LeftChild = currNode.MiddleChild;
+                currNode.RightChild = null;
+                currNode.LeftKey = currNode.RightKey;
+                currNode.RightKey = default;
+
+                return newNode;
+            }
+            else if (currNode.RightKey.CompareTo(node.LeftKey) >= 0)
+            {
+                node.MiddleChild = new TreeNode<T>(currNode.RightKey)
+                {
+                    LeftChild = node.MiddleChild,
+                    MiddleChild = currNode.RightChild
+                };
+
+                node.LeftChild = currNode;
+                currNode.RightKey = default;
+                currNode.RightChild = null;
+
+                return node;
+            }
+            else
+            {
+                var newNode = new TreeNode<T>(currNode.RightKey)
+                {
+                    LeftChild = currNode,
+                    MiddleChild = node
+                };
+
+                node.LeftChild = currNode.RightChild;
+                currNode.RightChild = null;
+                currNode.RightKey = default;
+
+                return newNode; 
+            }
+        }
+
+        public override String ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            RecursivePrint(this.root, sb);
+            return sb.ToString();
+        }
+
+        private void RecursivePrint(TreeNode<T> node, StringBuilder sb)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.LeftKey != null)
+            {
+                sb.Append(node.LeftKey).Append(" ");
+            }
+
+            if (node.RightKey != null)
+            {
+                sb.Append(node.RightKey).Append(Environment.NewLine);
+            }
+            else
+            {
+                sb.Append(Environment.NewLine);
+            }
+
+            if (node.IsTwoNode())
+            {
+                RecursivePrint(node.LeftChild, sb);
+                RecursivePrint(node.MiddleChild, sb);
+            }
+            else if (node.IsThreeNode())
+            {
+                RecursivePrint(node.LeftChild, sb);
+                RecursivePrint(node.MiddleChild, sb);
+                RecursivePrint(node.RightChild, sb);
+            }
+        }
+    }
+}
