2d9e9659736d700ab68e3eabb678fc876168fc65
Yordan Ivanov <yordangoshovivanov@gmail.com>
2022-12-27 20:18:00 +0200

    Add files via upload

---
+++ 06. Hash-Tables-Sets-and-Dictionaries-Exercise-Skeleton/01.RoyaleArena/RoyaleArena.cs
@@ -0,0 +1,129 @@
+namespace _01.RoyaleArena
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RoyaleArena : IArena
+    {
+        private Dictionary<int, BattleCard> cards = new Dictionary<int, BattleCard>();
+
+        public void Add(BattleCard card)
+        {
+            cards.Add(card.Id, card);
+        }
+
+        public bool Contains(BattleCard card)
+        {
+            return cards.ContainsKey(card.Id);
+        }
+
+        public int Count => cards.Count;
+
+        public void ChangeCardType(int id, CardType type)
+        {
+            if (!cards.ContainsKey(id))
+                throw new InvalidOperationException();
+
+            cards[id].Type = type; 
+        }
+
+        public BattleCard GetById(int id)
+        {
+            if (!cards.ContainsKey(id))
+                throw new InvalidOperationException();
+
+            return cards[id];
+        }
+
+        public void RemoveById(int id)
+        {
+            if (!cards.ContainsKey(id))
+                throw new InvalidOperationException();
+
+            cards.Remove(id);
+        }
+
+        public IEnumerable<BattleCard> GetByCardType(CardType type)
+        {
+            return cards.Values.Where(c => c.Type == type);
+        }
+
+        public IEnumerable<BattleCard> GetByTypeAndDamageRangeOrderedByDamageThenById(CardType type, int lo, int hi)
+        {
+            return cards.Values.Where(c => c.Type == type && c.Damage > lo && c.Damage < hi)
+                .OrderByDescending(c => c.Damage)
+                .ThenBy(c => c.Id);
+        }
+
+        public IEnumerable<BattleCard> GetByCardTypeAndMaximumDamage(CardType type, double damage)
+        {
+            var result = cards.Values.Where(c => c.Type == type && c.Damage <= damage)
+                .OrderByDescending(c => c.Damage)
+                .ThenBy(c => c.Id);
+
+            if (!result.Any())
+            {
+                throw new InvalidOperationException();
+            }
+
+            return result;
+        }
+
+        public IEnumerable<BattleCard> GetByNameOrderedBySwagDescending(string name)
+        {
+            var result = cards.Values.Where(c => c.Name == name)
+                .OrderByDescending(c => c.Swag)
+                .ThenBy(c => c.Id);
+
+            if (!result.Any())
+            {
+                throw new InvalidOperationException();
+            }
+
+            return result;
+        }
+
+        public IEnumerable<BattleCard> GetByNameAndSwagRange(string name, double lo, double hi)
+        {
+            var result = cards.Values.Where(c => c.Name == name && c.Swag >= lo && c.Swag < hi)
+                .OrderByDescending(c => c.Swag)
+                .ThenBy(c => c.Id);
+
+            if (!result.Any())
+            {
+                throw new InvalidOperationException();
+            }
+
+            return result;
+        }
+
+        public IEnumerable<BattleCard> FindFirstLeastSwag(int n)
+        {
+            if (cards.Count < n)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return cards.Values.OrderBy(c => c.Swag).ThenBy(c => c.Id).Take(n);
+        }
+
+        public IEnumerable<BattleCard> GetAllInSwagRange(double lo, double hi)
+        {
+            return cards.Values.Where(c => c.Swag >= lo && c.Swag <= hi)
+                .OrderBy(c => c.Swag);
+        }
+
+
+        public IEnumerator<BattleCard> GetEnumerator()
+        {
+            foreach (var item in cards)
+            {
+                yield return item.Value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ 06. Hash-Tables-Sets-and-Dictionaries-Exercise-Skeleton/02.WordCruncher/Program.cs
@@ -0,0 +1,82 @@
+﻿var syllables = Console.ReadLine().Split(", ").ToArray();
+var word = Console.ReadLine();
+var cruncher = new Cruncher(syllables, word);
+
+foreach (var path in cruncher.GetSyllablePaths())
+{
+    Console.WriteLine(path);
+}
+
+
+class Cruncher
+{
+    private List<Node> syllableGroups;
+
+    public Cruncher(string[] syllables, string word)
+    {
+        syllableGroups = new List<Node>();
+        syllableGroups = GenerateSyllableGroups(syllables, word);
+    }
+
+    private List<Node> GenerateSyllableGroups(string[] syllables, string word)
+    {
+        if (string.IsNullOrEmpty(word) || syllables.Length == 0)
+        {
+            return null;
+        }
+
+        var result = new List<Node>();
+
+        for (int i = 0; i < syllables.Length; i++)
+        {
+            var syllable = syllables[i];
+
+            if (word.StartsWith(syllable))
+            {
+                var nextSyllables = GenerateSyllableGroups(
+                    syllables.Where((_, index) => index != i).ToArray(),
+                    word.Substring(syllable.Length));
+
+                result.Add(new Node(syllable, nextSyllables));
+            }
+        }
+
+        return result;
+    }
+
+    public HashSet<string> GetSyllablePaths()
+    {
+        var allPaths = new List<List<string>>();
+        ReconstructsPaths(syllableGroups, new Stack<string>(), allPaths);
+
+        return new HashSet<string>(allPaths.Select(x => string.Join(' ', x)));
+    }
+
+    private void ReconstructsPaths(List<Node> nodes, Stack<string> currPath, List<List<string>> allPaths)
+    {
+        if (nodes == null)
+        {
+            allPaths.Add(new List<string>(currPath.Reverse()));
+            return;
+        }
+
+        foreach (var node in nodes)
+        {
+            currPath.Push(node.Syllable);
+            ReconstructsPaths(node.Syllables, currPath, allPaths);
+            currPath.Pop();
+        }
+    }
+
+    private class Node
+    {
+        public Node(string syllable, List<Node> nextSyllables)
+        {
+            Syllable = syllable;
+            Syllables = nextSyllables;
+        }
+
+        public string Syllable { get; set; }
+        public List<Node> Syllables { get; set; }
+    }
+}
